316511a1d7b3023abcd83ab698d43bf7664a0b11
Marcos Vinicius <vnimar.lima@gmail.com>
2024-04-17 19:50:45 -0300

    tentando subir o front 2.0

---
+++ sga-front/src/app/shared/card-login/card-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-login',
+  templateUrl: './card-login.component.html',
+  styleUrls: ['./card-login.component.scss']
+})
+export class CardLoginComponent {
+
+}
