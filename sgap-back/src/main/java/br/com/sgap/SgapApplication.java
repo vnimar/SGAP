db41ae4729d8e1e0ce2284f373a2137699d1001f
Rafael Aguiar Rodrigues <rafaelrodriguesaguiar18@gmail.com>
2024-05-16 18:12:38 -0300

    adicionando back-end

---
+++ sgap-back/src/main/java/br/com/sgap/SgapApplication.java
@@ -0,0 +1,13 @@
+package br.com.sgap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgapApplication.class, args);
+	}
+
+}
