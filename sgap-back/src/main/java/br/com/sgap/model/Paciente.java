8627570439b88605505c52a5837bc6e8e97d9b7a
Rafael Aguiar Rodrigues <rafaelrodriguesaguiar18@gmail.com>
2024-05-17 21:48:39 -0300

    criando novas models

---
+++ sgap-back/src/main/java/br/com/sgap/model/Paciente.java
@@ -0,0 +1,20 @@
+package br.com.sgap.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "paciente")
+@Table(name = "tb_paciente")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String email;
+    private String telefone;
+}
